33a2537a1c49540fadcf2cc58b5afc102abed0b4
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-20 16:54:42 +0200

    chore: Update favicon and background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Game from "./components/game/Game";
+import Logo from "./components/logo/Logo";
 
 export default function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <main className='h-screen grid place-items-center'>
+      <Logo />
+      <Game />
+    </main>
+  );
 }
---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+    return (
+        <img
+            src="/assets/logo.png"
+            alt="Logo Tech-Play Education"
+            className="absolute top-10 right-1 sm:right-3 md:right-5 lg:right-10 w-44 lg:w-52 xl:w-60"
+        />
+    );
+}
