3a4e87eeb20ac526c4830a8fee90e9ae9158e3d0
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-27 12:55:37 +0200

    feat: Update routing in App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Logo from './components/Logo/Logo';
 import Game from './components/Game/Game';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={
-          <main className='h-screen grid place-items-center'>
-            <Logo />
-            <Game />
-          </main>
-        } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={
+        <main className='h-screen grid place-items-center'>
+          <Logo />
+          <Game />
+        </main>
+      } />
+    </Routes>
   );
 }
